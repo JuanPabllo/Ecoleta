7605c07788cbebe96a8eb4301cadffe0cf3c4d0f
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-06-04 15:56:26 -0300

    conrreção 1

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const server = express()
+
+
+// Configurar pasta publica
+server.use(express.static("public"))
+
+
+//Ultilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache:  true
+})
+
+
+// Configurar caminhos da nossa aplicação
+// Página inicial
+// req: requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um titulo" })
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
+
+//Ligar o servidor
+server.listen(3000)
